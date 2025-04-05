9bbb2fe27727843577e320c4a78a383e9cb43bad
Shmuel-Smadar <34glass43@gmail.com>
2025-04-05 18:19:40 +0300

    change cursor on canvas when in fill mode

--- client/src/components/Drawing/Canvas.jsx
+++ client/src/components/Drawing/Canvas.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import useCanvas from '../../hooks/useCanvas'
+import BucketIcon from '../../assets/paint-bucket.png'
 import './Canvas.css'
 
 function Canvas({ client, userID, roomId }) {
@@ -14,10 +16,17 @@ function Canvas({ client, userID, roomId }) {
     handleTouchEnd
   } = useCanvas({ client, userID, roomId })
 
+  const isFillMode = useSelector(state => state.draw.isFillMode)
+
   return (
     <canvas
       ref={canvasRef}
       className="myCanvas"
+      style={{
+        cursor: isFillMode
+          ? `url(${BucketIcon}) 16 16, auto`
+          : 'crosshair'
+      }}
       onMouseDown={handleMouseDown}
       onMouseMove={handleMouseMove}
       onMouseUp={handleMouseUp}
