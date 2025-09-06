cc7021ed0fadd94b3920eee9bd66d2963c37bd79
Shmuel-Smadar <34glass43@gmail.com>
2025-09-06 21:02:47 +0300

    fix canvas styling on laptop

--- client/src/components/Drawing/useCanvasResize.js
+++ client/src/components/Drawing/useCanvasResize.js
@@ -1,16 +1,20 @@
-import { useEffect } from 'react';
-import { ASPECT_RATIO, CANVAS_WIDTH_RATIO, CANVAS_HEIGHT_RATIO } from '../../utils/constants';
+import { useEffect } from "react";
+import {
+  ASPECT_RATIO,
+  CANVAS_WIDTH_RATIO,
+  CANVAS_HEIGHT_RATIO,
+} from "../../utils/constants";
 
 /*
-* A hook that calculates the canvas size according to the window width and height.
-* it also listens to window size changes and update the canvas dimensions accordingly.
-*/
+ * A hook that calculates the canvas size according to the window width and height.
+ * it also listens to window size changes and update the canvas dimensions accordingly.
+ */
 export const useCanvasResize = (canvasRef) => {
   useEffect(() => {
     const resizeCanvas = () => {
       const canvas = canvasRef.current;
       if (!canvas) return;
-      const ctx = canvas.getContext('2d');
+      const ctx = canvas.getContext("2d");
       const maxWidth = window.innerWidth * CANVAS_WIDTH_RATIO;
       const maxHeight = window.innerHeight * CANVAS_HEIGHT_RATIO;
 
@@ -23,17 +27,18 @@ export const useCanvasResize = (canvasRef) => {
         height = width / ASPECT_RATIO;
       }
 
-      canvas.width = Math.min(width, maxWidth);
-      canvas.height = Math.min(height, maxHeight);
+      canvas.style.width = `${width}px`;
+      canvas.style.height = `${height}px`;
+      canvas.width = width;
+      canvas.height = height;
 
       ctx.lineWidth = 2;
-      ctx.lineCap = 'round';
+      ctx.lineCap = "round";
     };
 
-    window.addEventListener('resize', resizeCanvas);
+    window.addEventListener("resize", resizeCanvas);
     resizeCanvas();
 
-    return () => window.removeEventListener('resize', resizeCanvas);
+    return () => window.removeEventListener("resize", resizeCanvas);
   }, [canvasRef]);
-
 };
